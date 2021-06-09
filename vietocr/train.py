26368bf5512ba7218686e0b7306a2fd899131a9d
Nguyen Pham <phamduygiacnguyen9c@gmail.com>
2021-06-09 10:51:08 +0700

    Updated train.py

--- vietocr/train.py
+++ vietocr/train.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', required=True, help='see example at ')
     parser.add_argument('--checkpoint', required=False, help='your checkpoint')
+    parser.add_argument('--pretrained', required=False, help='using pretrained')
 
     args = parser.parse_args()
     config = Cfg.load_config_from_file(args.config)
@@ -21,8 +22,13 @@ def main():
     trainer = Trainer(config)
 
     if args.checkpoint:
+        print("Trong===============")
         trainer.load_checkpoint(args.checkpoint)
-        
+    print("Ngoai===============")
+    #
+    while True:
+        pass
+    #
     trainer.train()
 
 if __name__ == '__main__':
