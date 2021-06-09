8b55919595201c7e5424eff82fb4b126f915b185
Nguyen Pham <phamduygiacnguyen9c@gmail.com>
2021-06-09 11:01:08 +0700

    Updated train.py

--- vietocr/train.py
+++ vietocr/train.py
@@ -20,6 +20,7 @@ def main():
     #
 
     if args.pretrained == "False":
+        print("Using pretrained {args.config}: {args.pretrained}")
         trainer = Trainer(config, pretrained=False)
     else:
         trainer = Trainer(config)
